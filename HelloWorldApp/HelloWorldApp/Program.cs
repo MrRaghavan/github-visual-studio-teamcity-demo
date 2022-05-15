ed993c965bc6e510b127aea3fe0b4bf85a27b54a
MrRaghavan <raghavofbappa@gmail.com>
2022-05-15 23:11:26 +0530

    Trigger my first build on TeamCity

--- HelloWorldApp/HelloWorldApp/Program.cs
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -6,7 +6,9 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Universe!");
+            Console.WriteLine("Hello Universe this is Raghavan Mohan!");
+            Console.WriteLine("This change will trigger a build on TeamCity!");
+            Console.ReadLine();
         }
     }
 }
